088344190b90ca3d9077d76045e4b151323ed1b0
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-10-06 15:36:30 +0530

    Add: Welcome message

---
+++ src/main/java/com/bridgelabs/StackAndQueues/Stack.java
@@ -0,0 +1,13 @@
+package com.bridgelabs.StackAndQueues;
+
+/**
+ * Welcome message
+ *
+ */
+public class Stack
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome Message" );
+    }
+}
